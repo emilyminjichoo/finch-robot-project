13a2ef5958785ce85803680e4ecbc54d51a6eae6
Emily Cho <echo0043@mymail.lausd.net>
2025-06-04 10:22:51 -0700

    edit read me file and edit code

---
+++ src/main/java/com/Temaly/test.java
@@ -0,0 +1,28 @@
+package com.Temaly;
+
+import com.birdbrain.Finch;
+
+
+public class test {
+    public static void main(String[] args) {
+        Finch myFinch = new Finch();
+    
+//cupid shuffle patented
+
+
+        
+
+        // for (int i = 0; i < 10; i++) {
+        //     myFinch.setTurn("R", 180, 50);
+        // myFinch.setMove("B", 10, 75);
+        // myFinch.playNote(60,0.5);
+        // myFinch.playNote(var, 16);
+        // var+=10;
+        // }
+
+
+
+        myFinch.stopAll();
+        myFinch.disconnect();
+    }
+}
--- src/main/java/com/example/FinchDemo.java
+++ src/main/java/com/example/FinchDemo.java
@@ -11,9 +11,11 @@ public class FinchDemo {
         for (int i = 0; i < 10; i++) {
             myFinch.setBeak(100, 100, 100);
             myFinch.pause(1);
+            myFinch.setMove("Forward", 10.0, 50.0);
             myFinch.setBeak(0, 0, 0);
             myFinch.pause(1);
         }
+        myFinch.playNote(100, 16);
 
         myFinch.stopAll();
         myFinch.disconnect();
